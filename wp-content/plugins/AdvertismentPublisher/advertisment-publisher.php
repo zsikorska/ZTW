dac113a94d6a7845cde9ea55bca896dbfb93949b
Magdalena <magdawnowicka@gmail.com>
2023-03-01 12:13:51 +0100

    add advertisment publisher to admin settings

--- wp-content/plugins/AdvertismentPublisher/advertisment-publisher.php
+++ wp-content/plugins/AdvertismentPublisher/advertisment-publisher.php
@@ -7,60 +7,16 @@
 */
 
 
-function naph_admin_actions_register_menu(){
-    add_options_page("Advertisment Publisher", "New Post Highliter", 'manage_options', "naph", "naph_admin_page");
+function ad_pub_admin_actions_register_menu(){
+    add_options_page("Advertisment Publisher", "Advertisment Publisher", 'manage_options', "ad-pub", "ad_pub_admin_page");
 }
 
-add_action('admin_menu', 'naph_admin_actions_register_menu');
+add_action('admin_menu', 'ad_pub_admin_actions_register_menu');
 
-function naph_admin_page(){
-    // get _POST variable from globals
-    global $_POST;
-    // process changes from form
-    if(isset($_POST['naph_do_change'])){
-        if($_POST['naph_do_change'] == 'Y'){
-            $opDays = $_POST['naph_days'];
-            echo '<div class="notice notice-success is-dismissible"><p>Settings saved.</p></div>';
-            update_option('naph_days', $opDays);
-        }
-    }
-    //read current option value
-    $opDays = get_option('naph_days');
-    //display admin page
-    ?>
-    <div class="wrap">
-        <h1>Newly Added Post Highliter</h1>
-        <form name="naph_form" method="post">
-            <input type="hidden" name="naph_do_change" value="Y">
-            <p>Highlight post title for
-            <input type="number" name="naph_days" min="0" max="30" value="<?php echo $opDays ?>"> days
-            </p>
-            <p class="submit"><input type="submit" value="Submit"></p>
-        </form>
-    </div>
+function ad_pub_admin_page(){
+?>
+    <h1> Page to manage adds in progress...</h1>
 <?php
 }
 
-function naph_mark_new_post_title($content, $id){
-    //read post publish date
-    $date = new DateTime(get_the_date('Ymd', $id));
-    //get current date
-    $now =  new DateTime(date('Ymd'));
-    //get setting for how long post is a new post
-    $opDays = get_option('naph_days');//generate proper post title
-    if($date->diff($now)->format("%r%a") <= $opDays)
-        return $content."<sup class=\"naph_marker\">new</sup>";
-    return $content;
-}
-
-add_filter("the_title", "naph_mark_new_post_title", 10, 2);
-
-function naph_register_styles(){
-    //register style
-    wp_register_style('naph_styles', plugins_url('/css/style.css', __FILE__));
-    //enable style (load in meta of html)
-    wp_enqueue_style('naph_styles');
-}
- 
-add_action('init', 'naph_register_styles');
 ?>
