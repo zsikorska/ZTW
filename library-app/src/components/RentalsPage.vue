50366eb265627151a0265217554a8f6ec585dde5
zsikorska <sikorska.zuzia@gmail.com>
2023-04-03 18:13:47 +0200

    Added rental page (but there is problem with dates)

--- library-app/src/components/RentalsPage.vue
+++ library-app/src/components/RentalsPage.vue
@@ -1,3 +1,99 @@
 <template>
-  <h1>Rentals</h1>
+    <div id="app" class="small-container">
+        <h1>Rentals</h1>
+        <rental-form
+                :rentalDataSource="rentalData"
+                @add:rental="addRental"
+                @update:rental="updateRental"
+        />
+        <rentals-table
+                :rentalSource="rentals"
+                @delete:rental="deleteRental"
+                @update:rental="sendRentalData"
+        />
+    </div>
 </template>
+<script>
+import RentalsTable from "@/components/RentalsTable.vue";
+import RentalForm from "@/components/RentalForm.vue";
+
+export default {
+    name: "app",
+    components: {
+        RentalsTable,
+        RentalForm,
+    },
+    data() {
+        return {
+            rentals: [],
+            rentalData: {
+                book: null,
+                reader: null,
+                dateOfRental: "",
+                dateOfReturn: "",
+                returned: false,
+            },
+        };
+    },
+    methods: {
+        async getRentals() {
+            try {
+                const response = await fetch("http://localhost:8080/rentals");
+                const data = await response.json();
+                this.rentals = data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async addRental(rental) {
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(rental),
+            };
+
+            try {
+                const response = await fetch(
+                    "http://localhost:8080/rentals",
+                    requestOptions
+                );
+                const data = await response.json();
+                this.rentals = [...this.rentals, data];
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async updateRental(rental) {
+            const requestOptions = {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(rental),
+            };
+            try {
+                await fetch("http://localhost:8080/rentals/" + rental.id, requestOptions);
+                this.getRentals();
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async deleteRental(index) {
+            await fetch("http://localhost:8080/rentals/" + index, {
+                method: "DELETE",
+            });
+            try {
+                this.getRentals();
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        sendRentalData(rental) {
+            this.rentalData = rental;
+        },
+    },
+    mounted() {
+        this.getRentals();
+    },
+};
+</script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css">
+</style>
